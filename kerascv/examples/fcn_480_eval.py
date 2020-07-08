cfa33f5c52172fa84a8bd37be0f3d602acf12501
tanzhenyu <tanzheny@google.com>
2020-07-08 07:34:19 -0700

    Add meaniou.

--- kerascv/examples/fcn_480_eval.py
+++ kerascv/examples/fcn_480_eval.py
@@ -1,44 +1,27 @@
 import os
 import tensorflow as tf
-from tensorflow.keras import layers
 from kerascv.data.voc_segmentation import voc_segmentation_dataset_from_directory
 
 
-def get_fcn_32(weights_path, input_shape, n_classes=21):
-    keras_inp = tf.keras.Input(shape=input_shape, name="fcn_32s")
-    backbone = tf.keras.applications.vgg16.VGG16(include_top=False, weights="imagenet", input_tensor=keras_inp)
-    x = backbone.outputs[0]
-    x = layers.Conv2D(4096, 7, padding="same", activation="relu", name="fc6")(x)
-    x = layers.Dropout(0.5)(x)
-    x = layers.Conv2D(4096, 1, padding="same", activation="relu", name="fc7")(x)
-    x = layers.Dropout(0.5)(x)
-    x = layers.Conv2D(n_classes, 1, kernel_initializer="he_normal")(x)
-    conv_upsample = layers.Conv2DTranspose(n_classes, kernel_size=(64, 64), strides=(32, 32), use_bias=False,
-                                           padding="same")
-    x = conv_upsample(x)
-    x = layers.Activation("softmax")(x)
-    model = tf.keras.Model(keras_inp, x, name="fcn32_vgg16")
-    model.load_weights(weights_path)
-    return model
-
-
-def eval_fcn_32():
+def eval_fcn(weights_path):
     batch_size = 20
-    weights_path = os.path.join(os.getcwd(), 'fcn_32.hdf5')
-    eval_voc_ds_2012 = voc_segmentation_dataset_from_directory(split="val", batch_size=batch_size)
-    strategy = tf.distribute.MirroredStrategy()
-    with strategy.scope():
-        input_shape = (480, 480, 3)
-        iou_metric = tf.keras.metrics.MeanIoU(num_classes=21)
-        model = get_fcn_32(weights_path, input_shape)
-        y_pred = model.outputs[0]
-        y_pred = tf.math.argmax(y_pred, axis=-1)
-        inputs = model.inputs
-        eval_model = tf.keras.Model(inputs, y_pred)
-        eval_model.compile([iou_metric])
-    print('-------------------Start Evaluating-------------------')
-    eval_model.evaluate(eval_voc_ds_2012)
+    if os.path.exists(weights_path):
+        eval_voc_ds_2012 = voc_segmentation_dataset_from_directory(
+            split="val", batch_size=batch_size)
+        strategy = tf.distribute.MirroredStrategy()
+        with strategy.scope():
+            iou_metric = tf.keras.metrics.MeanIoU(num_classes=21)
+            model = tf.keras.models.load_model(weights_path, compile=False)
+            y_pred = model.outputs[0]
+            y_pred = tf.math.argmax(y_pred, axis=-1)
+            inputs = model.inputs
+            eval_model = tf.keras.Model(inputs, y_pred)
+            eval_model.compile(metrics=[iou_metric])
+        print('-------------------Start Evaluating {}-------------------'.format(weights_path))
+        eval_model.evaluate(eval_voc_ds_2012)
 
 
 if __name__ == "__main__":
-    eval_fcn_32()
+    eval_fcn(os.path.join(os.getcwd(), 'fcn_32.hdf5'))
+    eval_fcn(os.path.join(os.getcwd(), 'fcn_16.hdf5'))
+    eval_fcn(os.path.join(os.getcwd(), 'fcn_8.hdf5'))
--- kerascv/examples/fcn_480_train.py
+++ kerascv/examples/fcn_480_train.py
@@ -6,6 +6,24 @@ from tensorflow.keras import layers
 from kerascv.data.voc_segmentation import voc_segmentation_dataset_from_directory
 
 
+num_classes = 21
+input_shape = (480, 480, 3)
+
+
+class MyIOUMetrics(tf.keras.metrics.Metric):
+    def __init__(self, num_classes, name=None, **kwargs):
+        super(MyIOUMetrics, self).__init__(name=name, **kwargs)
+        self.mean_iou = tf.keras.metrics.MeanIoU(num_classes=num_classes)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        self.mean_iou.update_state(y_true, y_pred, sample_weight)
+
+    def result(self):
+        return self.mean_iou.result()
+
+
 def set_upsampling_weight(layer):
     kernel = layer.kernel
     kernel_shape = kernel.shape.as_list()
@@ -29,7 +47,7 @@ def set_upsampling_weight(layer):
     kernel.assign(weight)
 
 
-def get_fcn_32(input_shape, n_classes=21):
+def get_fcn_32():
     keras_inp = tf.keras.Input(shape=input_shape, name="fcn_32s")
     backbone = tf.keras.applications.vgg16.VGG16(include_top=False, weights="imagenet", input_tensor=keras_inp)
     x = backbone.outputs[0]
@@ -37,12 +55,34 @@ def get_fcn_32(input_shape, n_classes=21):
     x = layers.Dropout(0.5)(x)
     x = layers.Conv2D(4096, 1, padding="same", activation="relu", name="fc7")(x)
     x = layers.Dropout(0.5)(x)
-    x = layers.Conv2D(n_classes, 1, kernel_initializer="he_normal")(x)
-    conv_upsample = layers.Conv2DTranspose(n_classes, kernel_size=(64, 64), strides=(32, 32), use_bias=False, padding="same")
+    x = layers.Conv2D(num_classes, 1, kernel_initializer="he_normal", name="fcn_32_fc7_pool_conv_1")(x)
+    conv_upsample = layers.Conv2DTranspose(num_classes, kernel_size=(64, 64), strides=(32, 32),
+                                           use_bias=False, padding="same", name="fcn_32_conv2d_transpose_32")
     x = conv_upsample(x)
     set_upsampling_weight(conv_upsample)
     x = layers.Activation("softmax")(x)
     return tf.keras.Model(keras_inp, x, name="fcn32_vgg16")
+
+
+def get_fcn_16():
+    backbone = tf.keras.models.load_model('fcn_32.hdf5', compile=False)
+
+    o1 = backbone.get_layer("conv2d").output
+    conv_upsample = layers.Conv2DTranspose(num_classes, kernel_size=(4, 4), strides=(2, 2), use_bias=False,
+                                           padding="same", name="fcn_16_pool5_conv2d_transpose_2")
+    o1 = conv_upsample(o1)
+    set_upsampling_weight(conv_upsample)
+
+    o2 = backbone.get_layer("block4_pool").output
+    o2 = layers.Conv2D(num_classes, 1, kernel_initializer="he_normal", name="fcn_16_block4_pool_conv_1")(o2)
+
+    o = layers.Add()([o1, o2])
+    conv_upsample = layers.Conv2DTranspose(num_classes, kernel_size=(32, 32), strides=(16, 16),
+                                           use_bias=False, padding="same", name="fcn_16_conv2d_transpose_16")
+    o = conv_upsample(o)
+    set_upsampling_weight(conv_upsample)
+    o = layers.Activation("softmax")(o)
+    return tf.keras.Model(backbone.input, o, name="fcn16_vgg16")
 
 
 def train_val_save_fcn_32():
@@ -51,10 +91,10 @@ def train_val_save_fcn_32():
     eval_voc_ds_2012 = voc_segmentation_dataset_from_directory(split="val", batch_size=batch_size)
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
-        input_shape = (480, 480, 3)
         optimizer = tfa.optimizers.SGDW(weight_decay=0.0002, learning_rate=0.001, momentum=0.9)
-        model = get_fcn_32(input_shape)
-        model.compile(optimizer, "sparse_categorical_crossentropy", ["accuracy"])
+        model = get_fcn_32()
+        iou_metric = MyIOUMetrics(num_classes)
+        model.compile(optimizer, "sparse_categorical_crossentropy", weighted_metrics=["accuracy", iou_metric])
         ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath='fcn_32.hdf5', save_best_only=True)
         lr_callback = tf.keras.callbacks.ReduceLROnPlateau(patience=5, min_delta=0.01)
 
@@ -64,29 +104,8 @@ def train_val_save_fcn_32():
         print('var {}, {}'.format(var.name, var.shape))
     model.summary()
     # 2913 images is around 150 steps
-    model.fit(train_voc_ds_2012.prefetch(tf.data.experimental.AUTOTUNE), epochs=40,
+    model.fit(train_voc_ds_2012.prefetch(tf.data.experimental.AUTOTUNE), epochs=100,
               callbacks=[lr_callback, ckpt_callback], validation_data=eval_voc_ds_2012)
-
-
-def get_fcn_16(n_classes=21):
-    backbone = tf.keras.models.load_model('fcn_32.hdf5', compile=False)
-
-    o1 = backbone.get_layer("conv2d").output
-    conv_upsample = layers.Conv2DTranspose(n_classes, kernel_size=(4, 4), strides=(2, 2), use_bias=False,
-                                           padding="same", name="fcn_16_pool5_conv2d_transpose_2")
-    o1 = conv_upsample(o1)
-    set_upsampling_weight(conv_upsample)
-
-    o2 = backbone.get_layer("block4_pool").output
-    o2 = layers.Conv2D(n_classes, 1, kernel_initializer="he_normal", name="fcn_16_block4_pool_conv_1")(o2)
-
-    o = layers.Add()([o1, o2])
-    conv_upsample = layers.Conv2DTranspose(n_classes, kernel_size=(32, 32), strides=(16, 16), use_bias=False,
-                                           padding="same", name="fcn_16_conv2d_transpose_16")
-    o = conv_upsample(o)
-    set_upsampling_weight(conv_upsample)
-    o = layers.Activation("softmax")(o)
-    return tf.keras.Model(backbone.input, o, name="fcn16_vgg16")
 
 
 def train_val_save_fcn_16():
@@ -97,7 +116,8 @@ def train_val_save_fcn_16():
     with strategy.scope():
         optimizer = tfa.optimizers.SGDW(weight_decay=0.0002, learning_rate=0.0001, momentum=0.9)
         model = get_fcn_16()
-        model.compile(optimizer, "sparse_categorical_crossentropy", ["accuracy"])
+        iou_metric = MyIOUMetrics(num_classes)
+        model.compile(optimizer, "sparse_categorical_crossentropy", weighted_metrics=["accuracy", iou_metric])
         ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath='fcn_16.hdf5', save_best_only=True)
         lr_callback = tf.keras.callbacks.ReduceLROnPlateau(patience=3, min_delta=0.01)
 
@@ -107,7 +127,7 @@ def train_val_save_fcn_16():
         print('var {}, {}'.format(var.name, var.shape))
     model.summary()
     # 2913 images is around 150 steps
-    model.fit(train_voc_ds_2012.prefetch(tf.data.experimental.AUTOTUNE), epochs=10,
+    model.fit(train_voc_ds_2012.prefetch(tf.data.experimental.AUTOTUNE), epochs=40,
               callbacks=[lr_callback, ckpt_callback], validation_data=eval_voc_ds_2012)
 
 
