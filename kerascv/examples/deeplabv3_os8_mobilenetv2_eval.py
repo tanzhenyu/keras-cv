2981876725a2dd839ce552fc78db86eff2e1836c
tanzhenyu <tanzheny@google.com>
2020-07-12 08:13:29 -0700

    Using recall.

--- kerascv/examples/deeplabv3_os8_mobilenetv2_eval.py
+++ kerascv/examples/deeplabv3_os8_mobilenetv2_eval.py
@@ -7,12 +7,13 @@ class MyIOUMetrics(tf.keras.metrics.Metric):
     def __init__(self, class_id, num_classes, name=None, **kwargs):
         self.num_classes = num_classes
         self.class_id = class_id
-        self.acc_metric = tf.keras.metrics.BinaryAccuracy(threshold=0.3)
+        self.re_metric = tf.keras.metrics.Recall()
         super(MyIOUMetrics, self).__init__(name=name, **kwargs)
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         y_true = tf.one_hot(y_true, depth=self.num_classes)
         y_true = y_true[:, :, :, self.class_id]
+        y_pred = tf.nn.sigmoid(y_pred)
         y_pred = y_pred[:, :, :, self.class_id]
         self.acc_metric.update_state(y_true, y_pred, sample_weight)
 
@@ -33,9 +34,9 @@ def eval_deeplab(weights_path):
         iou_metrics = []
         for i in range(21):
             iou_metrics.append(MyIOUMetrics(class_id=i, num_classes=21, name='iou_' + str(i)))
-        top_k_metric = tf.keras.metrics.SparseTopKCategoricalAccuracy()
+        # top_k_metric = tf.keras.metrics.SparseTopKCategoricalAccuracy()
         model = tf.keras.models.load_model(weights_path, compile=False)
-        model.compile(metrics=["accuracy", top_k_metric] + iou_metrics)
+        model.compile(metrics=["accuracy"] + iou_metrics)
         print('-------------------Start Evaluating {}-------------------'.format(weights_path))
         model.evaluate(eval_voc_ds_2012)
 
