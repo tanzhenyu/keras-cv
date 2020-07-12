8832baf465c95f34803c594b84efcc45fc40efa9
tanzhenyu <tanzheny@google.com>
2020-07-12 07:59:57 -0700

    iou per class.

--- kerascv/examples/deeplabv3_os8_mobilenetv2_eval.py
+++ kerascv/examples/deeplabv3_os8_mobilenetv2_eval.py
@@ -3,13 +3,21 @@ import tensorflow as tf
 from kerascv.data.voc_segmentation import voc_segmentation_dataset_from_directory
 
 
-class MyIOUMetrics(tf.keras.metrics.MeanIoU):
-    def __init__(self, num_classes, name=None, **kwargs):
-        super(MyIOUMetrics, self).__init__(num_classes=num_classes, name=name, **kwargs)
+class MyIOUMetrics(tf.keras.metrics.Metric):
+    def __init__(self, class_id, num_classes, name=None, **kwargs):
+        self.num_classes = num_classes
+        self.class_id = class_id
+        self.acc_metric = tf.keras.metrics.BinaryAccuracy(threshold=0.3)
+        super(MyIOUMetrics, self).__init__(name=name, **kwargs)
 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_pred = tf.argmax(y_pred, axis=-1)
-        super(MyIOUMetrics, self).update_state(y_true, y_pred, sample_weight)
+        y_true = tf.one_hot(y_true, depth=self.num_classes)
+        y_true = y_true[:, :, :, self.class_id]
+        y_pred = y_pred[:, :, :, self.class_id]
+        self.acc_metric.update_state(y_true, y_pred, sample_weight)
+
+    def result(self):
+        return self.acc_metric.result()
 
 
 def eval_deeplab(weights_path):
@@ -22,10 +30,12 @@ def eval_deeplab(weights_path):
             split="val", batch_size=batch_size, base_size=base_size, crop_size=crop_size,
             preprocess_input=preprocess_input
         )
-        iou_metric = MyIOUMetrics(num_classes=21)
+        iou_metrics = []
+        for i in range(21):
+            iou_metrics.append(MyIOUMetrics(class_id=i, num_classes=21, name='iou_' + str(i)))
         top_k_metric = tf.keras.metrics.SparseTopKCategoricalAccuracy()
         model = tf.keras.models.load_model(weights_path, compile=False)
-        model.compile(metrics=["accuracy", iou_metric, top_k_metric])
+        model.compile(metrics=["accuracy", top_k_metric] + iou_metrics)
         print('-------------------Start Evaluating {}-------------------'.format(weights_path))
         model.evaluate(eval_voc_ds_2012)
 
